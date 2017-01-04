1af58d9c878ee3b7575c8b97d26906bb47e3c073
Marco Hinz <mh.codebro@gmail.com>
2017-01-04 16:42:54 +0100

    install: make shellcheck-clean

--- install.sh
+++ install.sh
@@ -9,62 +9,61 @@ Red='\033[0;31m'          # Red
 Blue='\033[0;34m'         # Blue
 
 need_cmd () {
-    if ! command -v "$1" > /dev/null 2>&1;then
-        printf "${Red}need '$1' (command not found)${Color_off}\n"
-        exit 0
+    if ! hash "$1" &>/dev/null; then
+        echo -e "${Red}need '$1' (command not found)${Color_off}"
+        exit 1
     fi
 }
 
 fetch_repo () {
-    if [ -d "${HOME}/.SpaceVim" ];then
-        cd ${HOME}/.SpaceVim
-        git pull
-        ret="$?"
-        printf "${Blue}Successfully update SpaceVim${Color_off}\n"
+    if [[ -d "$HOME/.SpaceVim" ]]; then
+        git -C "$HOME/.SpaceVim" pull
+        ret=$?
+        echo -e "${Blue}Successfully update SpaceVim${Color_off}"
     else
-        git clone https://github.com/SpaceVim/SpaceVim.git ${HOME}/.SpaceVim
-        ret="$?"
-        printf "${Blue}Successfully clone SpaceVim${Color_off}\n"
+        git clone https://github.com/SpaceVim/SpaceVim.git "$HOME/.SpaceVim"
+        ret=$?
+        echo -e "${Blue}Successfully clone SpaceVim${Color_off}"
     fi
 }
 
 install_vim () {
-    if [ -f "${HOME}/.vimrc" ];then
-        mv ${HOME}/.vimrc ${HOME}/.vimrc_back
-        ret="$?"
-        printf "${Blue}BackUp ${HOME}/.vimrc${Color_off}\n"
+    if [[ -f "$HOME/.vimrc" ]]; then
+        mv "$HOME/.vimrc" "$HOME/.vimrc_back"
+        ret=$?
+        echo -e "${Blue}BackUp $HOME/.vimrc${Color_off}"
     fi
 
-    if [ -d "${HOME}/.vim" ];then
-        if file ${HOME}/.vim | grep ${HOME}/.SpaceVim &> /dev/null;then
-            printf "${Blue}Installed SpaceVim for vim${Color_off}\n"
+    if [[ -d "$HOME/.vim" ]]; then
+        if file "$HOME/.vim" | grep "$HOME/.SpaceVim" &>/dev/null; then
+            echo -e "${Blue}Installed SpaceVim for vim${Color_off}"
         else
-            mv ${HOME}/.vim ${HOME}/.vim_back
-            ret="$?"
-            printf "${Blue}BackUp ${HOME}/.vim${Color_off}\n"
-            ln -s ${HOME}/.SpaceVim ${HOME}/.vim
-            printf "${Blue}Installed SpaceVim for vim${Color_off}\n"
+            mv "$HOME/.vim" "$HOME/.vim_back"
+            ret=$?
+            echo -e "${Blue}BackUp $HOME/.vim${Color_off}"
+            ln -s "$HOME/.SpaceVim" "$HOME/.vim"
+            echo -e "${Blue}Installed SpaceVim for vim${Color_off}"
         fi
     else
-        ln -s ${HOME}/.SpaceVim ${HOME}/.vim
-        printf "${Blue}Installed SpaceVim for vim${Color_off}\n"
+        ln -s "$HOME/.SpaceVim" "$HOME/.vim"
+        echo -e "${Blue}Installed SpaceVim for vim${Color_off}"
     fi
 }
 
 install_neovim () {
-    if [ -d "${HOME}/.config/nvim" ];then
-        if file ${HOME}/.config/nvim | grep ${HOME}/.SpaceVim &> /dev/null;then
-            printf "${Blue}Installed SpaceVim for neovim${Color_off}\n"
+    if [[ -d "$HOME/.config/nvim" ]]; then
+        if file "$HOME/.config/nvim" | grep "$HOME/.SpaceVim" &>/dev/null; then
+            echo -e "${Blue}Installed SpaceVim for neovim${Color_off}"
         else
-            mv ${HOME}/.config/nvim ${HOME}/.config/nvim_back
-            ret="$?"
-            printf "${Blue}BackUp ${HOME}/.config/nvim${Color_off}\n"
-            ln -s ${HOME}/.SpaceVim ${HOME}/.config/nvim
-            printf "${Blue}Installed SpaceVim for neovim${Color_off}\n"
+            mv "$HOME/.config/nvim" "$HOME/.config/nvim_back"
+            ret=$?
+            echo -e "${Blue}BackUp $HOME/.config/nvim${Color_off}"
+            ln -s "$HOME/.SpaceVim" "$HOME/.config/nvim"
+            echo -e "${Blue}Installed SpaceVim for neovim${Color_off}"
         fi
     else
-        ln -s ${HOME}/.SpaceVim ${HOME}/.config/nvim
-        printf "${Blue}Installed SpaceVim for neovim${Color_off}\n"
+        ln -s "$HOME/.SpaceVim" "$HOME/.config/nvim"
+        echo -e "${Blue}Installed SpaceVim for neovim${Color_off}"
     fi
 }
 
