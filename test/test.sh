e9ead2f5ca108e4acde222b0032310f4fd65c453
Daniel Hahler <git@thequod.de>
2017-10-01 17:06:18 +0200

    tests: rename test/test.vim to test/vimrc for clarity

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
 if [ ! -e /tmp/vader ]; then
     git clone https://github.com/junegunn/vader.vim.git /tmp/vader
 fi
-vim -Nu test/test.vim -c 'Vader! test/**'
+vim -Nu test/vimrc -c 'Vader! test/**'
