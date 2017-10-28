da02449db849f085469b460a0bff7a557ab16c01
wsdjeg <wsdjeg@163.com>
2017-10-28 14:59:16 +0800

    Change remote name

--- wiki/async.sh
+++ wiki/async.sh
@@ -5,7 +5,7 @@ cd wiki/
 git init 
 git add .
 git commit -m "Update wiki"
-git remote add origin https://github.com/SpaceVim/SpaceVim.wiki.git
-git push -f -u origin master 
+git remote add SpaceVimWiki https://github.com/SpaceVim/SpaceVim.wiki.git
+git push -f -u SpaceVimWiki master 
 cd ..
 rm -rf ./wiki/.git/
