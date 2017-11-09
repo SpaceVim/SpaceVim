870d4be8bc7c1230758bec90f0007d55ee0a0531
wsdjeg <wsdjeg@163.com>
2017-11-09 23:59:15 +0800

    Update comment

--- .ci/after_failure.sh
+++ .ci/after_failure.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-    echo "This is after failure script"
-    echo "The repo name is "${TRAVIS_REPO_SLUG}
-    echo "The pr number is "${TRAVIS_PULL_REQUEST}
-    echo $TRAVIS_TEST_RESULT
+    source .ci/common/github_commenter
+    if [ "$LINT" = "vimlint-errors" ] ; then
+        add_comment_to_pr $TRAVIS_TEST_RESULT
+    elif [ "$LINT" = "vint-errors" ] ; then
+        add_comment_to_pr $TRAVIS_TEST_RESULT
+    elif [ "$LINT" = "vader" ] ; then
+        add_comment_to_pr $TRAVIS_TEST_RESULT
+    fi
 fi
