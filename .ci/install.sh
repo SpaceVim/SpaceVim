e9201a5d165efa1b81b0fe655ade8d8066d30416
wsdjeg <wsdjeg@163.com>
2017-11-10 19:40:49 +0800

    Use neovim report error

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
-set -e
+# Fail on unset variables and command errors
+set -ue -o pipefail
+
+# Prevent commands misbehaving due to locale differences
+export LC_ALL=C
+
+# Get root path of the script
+root=$(cd $(dirname $0); pwd)
+
+. $root/install/linux.sh
+
+# Install Vim/Neovim
+install $VIM $VIM_VERSION
+
 if [ "${LINT#vimlint}" != "$LINT" ]; then
     git clone --depth=1 https://github.com/syngan/vim-vimlint /tmp/vimlint
     git clone --depth=1 https://github.com/ynkdir/vim-vimlparser /tmp/vimlparser
-    C_OPTS="--prefix=$DEPS --with-features=huge --disable-gui --enable-pythoninterp"
-    (git clone --depth 1 https://github.com/vim/vim /tmp/vim &&
-        cd /tmp/vim &&
-        ./configure $C_OPTS &&
-        make install)
 elif [ "${LINT#vint}" != "$LINT" ]; then
     virtualenv /tmp/vint && source /tmp/vint/bin/activate && pip install vim-vint
 elif [ "${LINT#vader}" != "$LINT" ]; then
     git clone --depth=1 https://github.com/Shougo/dein.vim.git ~/.cache/vimfiles/repos/github.com/Shougo/dein.vim
-    C_OPTS="--prefix=$DEPS --with-features=huge --disable-gui --enable-pythoninterp"
-    (git clone --depth 1 https://github.com/vim/vim /tmp/vim &&
-        cd /tmp/vim &&
-        ./configure $C_OPTS &&
-        make install)
 fi
