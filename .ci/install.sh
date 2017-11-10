1ec2ed92ad21f2567e48e59851ff66869087dfb1
wsdjeg <wsdjeg@163.com>
2017-11-10 14:36:02 +0800

    Install vim before test

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,7 +4,11 @@ set -e
 if [ "${LINT#vimlint}" != "$LINT" ]; then
     git clone --depth=1 https://github.com/syngan/vim-vimlint /tmp/vimlint
     git clone --depth=1 https://github.com/ynkdir/vim-vimlparser /tmp/vimlparser
-    sudo apt-get install -y vim-gtk
+    C_OPTS="--prefix=$DEPS --with-features=huge --disable-gui --enable-pythoninterp"
+    (git clone --depth 1 https://github.com/vim/vim /tmp/vim &&
+        cd /tmp/vim &&
+        ./configure $C_OPTS &&
+        make install)
 elif [ "${LINT#vint}" != "$LINT" ]; then
     virtualenv /tmp/vint && source /tmp/vint/bin/activate && pip install vim-vint
 elif [ "${LINT#vader}" != "$LINT" ]; then
