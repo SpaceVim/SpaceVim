53a3ae6142dd3755bf3db2d68c6d47229d12dad0
wsdjeg <wsdjeg@163.com>
2017-11-20 01:09:34 +0800

    Fix install

--- .ci/install/linux.sh
+++ .ci/install/linux.sh
@@ -33,7 +33,7 @@ install_nvim() {
     CMAKE_EXTRA_FLAGS="-DTRAVIS_CI_BUILD=ON -DCMAKE_INSTALL_PREFIX:PATH=$out"
   make install
   python -m pip install --user neovim
-  python3 -m pip3 install --user neovim
+  python3 -m pip install --user neovim
   ln -sf $out $HOME/vim
 }
 
