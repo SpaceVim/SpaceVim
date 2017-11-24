dd2562934171b77885b888f8fb18311d9d1858f4
Wang Shidong <wsdjeg@users.noreply.github.com>
2017-11-24 17:37:24 +0800

    Fix windows installation

--- docs/install.cmd
+++ docs/install.cmd
@@ -46,4 +46,15 @@ echo ""
 echo "That's it. Thanks for installing $app_name. Enjoy!"
 echo ""
 
-pause
+Function Pause ($Message = "Press any key to continue . . . ") {
+    if ((Test-Path variable:psISE) -and $psISE) {
+        $Shell = New-Object -ComObject "WScript.Shell"
+        $Button = $Shell.Popup("Click OK to continue.", 0, "Script Paused", 0)
+    } else {     
+        Write-Host -NoNewline $Message
+        [void][System.Console]::ReadKey($true)
+        Write-Host
+    }
+}
+
+Pause
