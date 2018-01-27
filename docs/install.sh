3baba2228f504d74b9eda8679338efe889114588
wsdjeg <wsdjeg@outlook.com>
2018-01-27 20:02:48 +0800

    Update colors

--- docs/install.sh
+++ docs/install.sh
@@ -9,15 +9,82 @@
 #=============================================================================
 
 # Init option {{{
-# Reset
 Color_off='\033[0m'       # Text Reset
-Version='0.6.0'
 
+# terminal color template {{{
 # Regular Colors
-Red='\033[0;31m'
-Blue='\033[0;34m'
-Green='\033[0;32m'
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
 
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+# }}}
+
+# version
+Version='0.6.0'
 #System name
 System="$(uname -s)"
 
@@ -42,7 +109,7 @@ success() {
 }
 
 info() {
-  msg "${Blue}==>${Color_off} ${1}${2}"
+  msg "${Blue}[➭]${Color_off} ${1}${2}"
 }
 
 error() {
@@ -52,6 +119,12 @@ error() {
 
 warn () {
   msg "${Red}[✘]${Color_off} ${1}${2}"
+}
+# }}}
+
+# echo_with_color {{{
+echo_with_color () {
+  printf '%b\n' "$1$2" >&2
 }
 # }}}
 
@@ -227,33 +300,34 @@ usage () {
 # install_done {{{
 
 install_done () {
-  echo ""
-  echo "Almost done!"
-  echo "=============================================================================="
-  echo "==        Open Vim and it will install the plugins automatically           =="
-  echo "=============================================================================="
-  echo ""
-  echo "That's it. Thanks for installing SpaceVim. Enjoy!"
-  echo ""
+  echo_with_color ${Yellow} ""
+  echo_with_color ${Yellow} "Almost done!"
+  echo_with_color ${Yellow} "=============================================================================="
+  echo_with_color ${Yellow} "==    Open Vim or Neovim and it will install the plugins automatically      =="
+  echo_with_color ${Yellow} "=============================================================================="
+  echo_with_color ${Yellow} ""
+  echo_with_color ${Yellow} "That's it. Thanks for installing SpaceVim. Enjoy!"
+  echo_with_color ${Yellow} ""
 }
 
 # }}}
 
 # welcome {{{
 
+
 welcome () {
-  echo "        /######                                     /##    /##/##             "
-  echo "       /##__  ##                                   | ##   | #|__/             "
-    echo "      | ##  \__/ /######  /######  /####### /######| ##   | ##/##/######/#### "
-    echo "      |  ###### /##__  ##|____  ##/##_____//##__  #|  ## / ##| #| ##_  ##_  ##"
-    echo "       \____  #| ##  \ ## /######| ##     | ########\  ## ##/| #| ## \ ## \ ##"
-    echo "       /##  \ #| ##  | ##/##__  #| ##     | ##_____/ \  ###/ | #| ## | ## | ##"
-    echo "      |  ######| #######|  ######|  ######|  #######  \  #/  | #| ## | ## | ##"
-    echo "       \______/| ##____/ \_______/\_______/\_______/   \_/   |__|__/ |__/ |__/"
-    echo "               | ##                                                           "
-    echo "               | ##                                                           "
-    echo "               |__/                                                           "
-  echo "                      version : 0.7.0-dev       by : spacevim.org             "
+    echo_with_color ${Yellow} "        /######                                     /##    /##/##             "
+    echo_with_color ${Yellow} "       /##__  ##                                   | ##   | #|__/             "
+    echo_with_color ${Yellow} "      | ##  \__/ /######  /######  /####### /######| ##   | ##/##/######/#### "
+    echo_with_color ${Yellow} "      |  ###### /##__  ##|____  ##/##_____//##__  #|  ## / ##| #| ##_  ##_  ##"
+    echo_with_color ${Yellow} "       \____  #| ##  \ ## /######| ##     | ########\  ## ##/| #| ## \ ## \ ##"
+    echo_with_color ${Yellow} "       /##  \ #| ##  | ##/##__  #| ##     | ##_____/ \  ###/ | #| ## | ## | ##"
+    echo_with_color ${Yellow} "      |  ######| #######|  ######|  ######|  #######  \  #/  | #| ## | ## | ##"
+    echo_with_color ${Yellow} "       \______/| ##____/ \_______/\_______/\_______/   \_/   |__|__/ |__/ |__/"
+    echo_with_color ${Yellow} "               | ##                                                           "
+    echo_with_color ${Yellow} "               | ##                                                           "
+    echo_with_color ${Yellow} "               |__/                                                           "
+    echo_with_color ${Yellow} "                      version : 0.7.0-dev       by : spacevim.org             "
 }
 
 # }}}
@@ -324,6 +398,7 @@ main () {
         exit 0
         ;;
       --install|-i)
+        welcome
         need_cmd 'git'
         fetch_repo
         if [ $# -eq 2 ]
@@ -331,15 +406,18 @@ main () {
           case $2 in
             neovim)
               install_neovim
+              install_done
               exit 0
               ;;
             vim)
               install_vim
+              install_done
               exit 0
           esac
         fi
         install_vim
         install_neovim
+        install_done
         exit 0
         ;;
       --help|-h)
