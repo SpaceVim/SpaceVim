e2d5da41b2165b696bb372daca6d4cbc7896b761
wsdjeg <wsdjeg@outlook.com>
2018-01-27 20:16:24 +0800

    Remove git-dir

--- docs/install.sh
+++ docs/install.sh
@@ -132,7 +132,9 @@ echo_with_color () {
 fetch_repo () {
   if [[ -d "$HOME/.SpaceVim" ]]; then
     info "Trying to update SpaceVim"
-    git --git-dir "$HOME/.SpaceVim/.git" pull
+    cd "$HOME/.SpaceVim"
+    git pull
+    cd - > /dev/null 2>&1
     success "Successfully update SpaceVim"
   else
     info "Trying to clone SpaceVim"
