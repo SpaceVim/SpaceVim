9e811c23717c9f5545ba76280aad7a376132b7be
wsdjeg <wsdjeg@outlook.com>
2018-02-02 20:08:36 +0800

    Fix indent

--- docs/cn/install.cmd
+++ docs/cn/install.cmd
@@ -39,14 +39,14 @@ sleep 1
 echo "==> 测试 git 命令"
 if (Get-Command "git" -ErrorAction SilentlyContinue) {
   git version
-    echo "[OK] 测试成功. 开始下一个测试..."
-    sleep 1
+  echo "[OK] 测试成功. 开始下一个测试..."
+  sleep 1
 } else {
   echo ""
-    echo "[ERROR] 无法在你的 PATH 中发现 'git.exe' 命令"
-    echo ">>> 准备退出......"
-    Pause
-    exit
+  echo "[ERROR] 无法在你的 PATH 中发现 'git.exe' 命令"
+  echo ">>> 准备退出......"
+  Pause
+  exit
 }
 
 echo ""
@@ -54,13 +54,13 @@ echo ""
 echo "==> 测试 gvim 命令"
 if (Get-Command "gvim" -ErrorAction SilentlyContinue) {
   echo ($(vim --version) -split '\n')[0]
-    echo "[OK] 测试成功. 开始下一个测试..."
-    sleep 1
+  echo "[OK] 测试成功. 开始下一个测试..."
+  sleep 1
 } else {
-  echo "[WARNING] 无法在你的 PATH 中发现 'gvim.exe' 命令. 安装已继续..."
-    echo ""
-    echo "[WARNING] 请后续安装 gvim 或者正确设置你的 PATH! "
-    Pause
+  echo "[WARNING] 无法在你的 PATH 中发现 'gvim.exe' 命令. 但仍可继续安装..."
+  echo ""
+  echo "[WARNING] 请后续安装 gvim 或者正确设置你的 PATH! "
+  Pause
 }
 
 echo "<== 环境依赖检测已完成. 继续下一步..."
@@ -70,31 +70,31 @@ echo ""
 
 if (!(Test-Path "$HOME\.SpaceVim")) {
   echo "==> 正在安装 $app_name"
-    git clone $repo_url $repo_path
+  git clone $repo_url $repo_path
 } else {
   echo "==> 正在更新 $app_name"
-    Push-Location $repo_path
-    git pull origin master
+  Push-Location $repo_path
+  git pull origin master
 }
 
 echo ""
 if (!(Test-Path "$HOME\vimfiles")) {
   cmd /c mklink $HOME\vimfiles $repo_path
-    echo "[OK] 已为 vim 安装 SpaceVim"
-    sleep 1
+  echo "[OK] 已为 vim 安装 SpaceVim"
+  sleep 1
 } else {
   echo "[OK] $HOME\vimfiles 已存在"
-    sleep 1
+  sleep 1
 }
 
 echo ""
 if (!(Test-Path "$HOME\AppData\Local\nvim")) {
   cmd /c mklink $HOME\AppData\Local\nvim $repo_path
-    echo "[OK] 已为 neovim 安装 SpaceVim"
-    sleep 1
+  echo "[OK] 已为 neovim 安装 SpaceVim"
+  sleep 1
 } else {
   echo "[OK] $HOME\AppData\Local\nvim 已存在"
-    sleep 1
+  sleep 1
 }
 
 echo ""
