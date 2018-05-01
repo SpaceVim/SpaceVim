50333abddf9467f7813957f5401fea1c47f8f45d
Wang Shidong <wsdjeg@outlook.com>
2018-05-01 21:54:49 +0800

    Fix detach script (#1684)

--- .ci/detach_plugin.sh
+++ .ci/detach_plugin.sh
@@ -41,16 +41,20 @@ main () {
             git clone https://github.com/wsdjeg/dein-ui.vim.git detach/$1
             cd detach/$1
             _checkdir autoload/SpaceVim/api
+            _checkdir autoload/SpaceVim/api/data
             _checkdir autoload/SpaceVim/api/vim
             _checkdir autoload/SpaceVim/mapping
             _checkdir autoload/SpaceVim/plugins
             _detect autoload/SpaceVim/plugins/manager.vim
             _detect autoload/SpaceVim/api.vim
+            _detect autoload/SpaceVim/api/job.vim
+            _detect autoload/SpaceVim/api/data/list.vim
+            _detect autoload/SpaceVim/api/vim/compatible.vim
             _detect LICENSE
             git add .
             git commit -m "Auto Update"
-            git remote add wsdjeg_flygrep https://SpaceVimBot:${BOTSECRET}@github.com/wsdjeg/FlyGrep.vim.git
-            git push wsdjeg_flygrep master 
+            git remote add wsdjeg_dein_ui https://SpaceVimBot:${BOTSECRET}@github.com/wsdjeg/dein-ui.vim.git
+            git push wsdjeg_dein_ui master 
             cd -
             rm -rf detach/$1
             exit 0
