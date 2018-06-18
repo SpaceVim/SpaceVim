e76540e0fcccce2fcbc9f439c2498611a2b05d22
Wang Shidong <wsdjeg@outlook.com>
2018-06-18 15:53:47 +0800

    Update version: 0.9.0-dev (#1842)
    * New version 0.9.0-dev
    
    * Update release script

--- docs/install.sh
+++ docs/install.sh
@@ -84,7 +84,7 @@ On_IWhite='\033[0;107m'   # White
 # }}}
 
 # version
-Version='0.8.0'
+Version='0.9.0-dev'
 #System name
 System="$(uname -s)"
 
@@ -330,7 +330,7 @@ welcome () {
     echo_with_color ${Yellow} "               | ##                                                           "
     echo_with_color ${Yellow} "               | ##                                                           "
     echo_with_color ${Yellow} "               |__/                                                           "
-    echo_with_color ${Yellow} "                      version : 0.8.0           by : spacevim.org             "
+    echo_with_color ${Yellow} "                      version : 0.9.0-dev       by : spacevim.org             "
 }
 
 # }}}
