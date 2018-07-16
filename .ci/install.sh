eda5c363742529954eceb0c9372385e81696e248
wsdjeg <wsdjeg@outlook.com>
2018-07-16 22:07:31 +0800

    Fix lint

--- .ci/install.sh
+++ .ci/install.sh
@@ -17,10 +17,12 @@ elif [ "${LINT#vint}" != "$LINT" ]; then
     pip install vim-vint pathlib enum34 typing
 elif [ "${LINT#vader}" != "$LINT" ]; then
     git clone --depth=1 https://github.com/Shougo/dein.vim.git ~/.cache/vimfiles/repos/github.com/Shougo/dein.vim
-    mkdir "${DEPS}/_neovim"
-    wget -q -O - https://github.com/neovim/neovim/releases/download/nightly/nvim-${TRAVIS_OS_NAME}64.tar.gz \
-        | tar xzf - --strip-components=1 -C "${DEPS}/_neovim"
+    if [[ ! -d "${DEPS}/_neovim" ]]; then
+        mkdir -p "${DEPS}/_neovim"
+        wget -q -O - https://github.com/neovim/neovim/releases/download/nightly/nvim-${TRAVIS_OS_NAME}64.tar.gz \
+            | tar xzf - --strip-components=1 -C "${DEPS}/_neovim"
 
+    fi
     export PATH="${DEPS}/_neovim/bin:${PATH}"
     echo "\$PATH: \"${PATH}\""
 
