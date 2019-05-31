f0f54204568fe696a5c85a5df1113e461515509f
Shidong Wang <wsdjeg@outlook.com>
2019-05-31 21:26:43 +0800

    Update lua version

--- lua/spacevim/opt.lua
+++ lua/spacevim/opt.lua
@@ -1,8 +1,13 @@
 local options = {}
 
+options._opts = {}
+
 
 function options.init()
-    
+    options._opts.version = '1.2.0-dev'
+    -- Change the default indentation of SpaceVim, default is 2.
+    options._opts.default_indent = 2
+    options._opts.expand_tab = true
 end
 
 
