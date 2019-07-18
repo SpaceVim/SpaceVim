e02ef31f72ca598191167fd0ce004a4bae9bfe12
wsdjeg <wsdjeg@163.com>
2019-07-18 21:06:03 +0800

    Update version

--- docs/install.cmd
+++ docs/install.cmd
@@ -11,7 +11,7 @@ echo "       \______/| ##____/ \_______/\_______/\_______/   \_/   |__|__/ |__/ 
 echo "               | ##                                                           "
 echo "               | ##                                                           "
 echo "               |__/                                                           "
-echo "                      version : 1.2.0-dev       by : spacevim.org             "
+echo "                      version : 1.2.0           by : spacevim.org             "
 
 Push-Location ~
 
