0928e219d0b1bdc1124fbbbdfe09261c40d36070
wsdjeg <wsdjeg@outlook.com>
2019-10-09 21:07:44 +0800

    Do not need docker

--- .ci/install.sh
+++ .ci/install.sh
@@ -6,7 +6,6 @@ set -ue -o pipefail
 # Prevent commands misbehaving due to locale differences
 export LC_ALL=C
 
-docker pull spacevim/vims
 git fetch origin master:master
 
 if [ "${LINT#vimlint}" != "$LINT" ]; then
