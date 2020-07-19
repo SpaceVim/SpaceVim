1cecc6833554e50403a52e515d33bd63f5667ce3
Shidong Wang <wsdjeg@outlook.com>
2020-07-19 16:44:22 +0800

    Fix

--- lua/spacevim/neovim/buffer.lua
+++ lua/spacevim/neovim/buffer.lua
@@ -3,7 +3,7 @@ local M = {}
 local mt = {
     __index = function(table, key)
         return vim.api.nvim_buf_get_lines(table.bufnr, key - 1, key, 0)[1];
-    end
+    end;
     __newindex = function(table, key, value)
         vim.api.nvim_buf_set_lines(table.bufnr, key -1, key, {value});
     end
