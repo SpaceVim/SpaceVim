73abb1cae4e69a6c178b67e258845904b5ab53b5
Shidong Wang <wsdjeg@outlook.com>
2020-07-19 16:43:18 +0800

    Update

--- lua/spacevim/neovim/buffer.lua
+++ lua/spacevim/neovim/buffer.lua
@@ -4,6 +4,9 @@ local mt = {
     __index = function(table, key)
         return vim.api.nvim_buf_get_lines(table.bufnr, key - 1, key, 0)[1];
     end
+    __newindex = function(table, key, value)
+        vim.api.nvim_buf_set_lines(table.bufnr, key -1, key, {value})
+    end
 }
 
 setmetatable(M, mt);
