154510d15a8a33455e6a7cd23b6ee75b4d66f6c1
Wang Shidong <wsdjeg@outlook.com>
2020-10-11 22:19:13 +0800

    Fix flygrep detach script (#3891)

--- .ci/detach_plugin.sh
+++ .ci/detach_plugin.sh
@@ -43,11 +43,118 @@ main () {
             _detect autoload/SpaceVim/api/data/json.vim
             _detect autoload/SpaceVim/api/prompt.vim
             _detect autoload/SpaceVim/api/job.vim
+            _detect autoload/SpaceVim/api/file.vim
             _detect autoload/SpaceVim/api/system.vim
             _detect autoload/SpaceVim/mapping/search.vim
             _detect autoload/SpaceVim/logger.vim
             _detect syntax/SpaceVimFlyGrep.vim
+            cat <<EOT > README.md
+
+# FlyGrep.vim
+> Asynchronously fly grep in vim
+
+This plugin is automatically detach from [SpaceVim](https://github.com/SpaceVim/SpaceVim/). you can use it without SpaceVim.
+
+![searching project](https://user-images.githubusercontent.com/13142418/35278709-7856ed62-0010-11e8-8b1e-e6cc6374b0dc.gif)
+
+## Install
+
+for dein.vim
+
+```vim
+call dein#add('wsdjeg/FlyGrep.vim')
+```
+
+for vim-plug
+
+```vim
+Plug 'wsdjeg/FlyGrep.vim'
+```
+
+## usage
+
+```
+:FlyGrep
+```
+
+you also can define custom mapping, for example:
+
+```vim
+nnoremap <Space>s/ :FlyGrep<cr>
+```
+
+Key Binding |	Description
+-----------| -----------
+`SPC s /` | Searching in project on the fly with default tools
+
+key binding in FlyGrep buffer:
+
+Key Binding |	Description
+-----------| -----------
+`<Esc>` | close FlyGrep buffer
+`<C-c>` | close FlyGrep buffer
+`<Enter>` | open file at the cursor line
+`<Tab>` | move cursor line down
+`<C-j>` | move cursor line down
+`<S-Tab>` | move cursor line up
+`<C-k>` | move cursor line up
+`<Bs>` | remove last character
+`<C-w>` | remove the Word before the cursor
+`<C-u>` | remove the Line before the cursor
+`<C-k>` | remove the Line after the cursor
+`<C-a>`/`<Home>` | Go to the beginning of the line
+`<C-e>`/`<End>` | Go to the end of the line
+EOT
             _detect LICENSE
+            _checkdir plugin
+            cat <<EOT > plugin/FlyGrep.vim
+"=============================================================================
+" FlyGrep.vim --- Fly grep in vim
+" Copyright (c) 2016-2017 Shidong Wang & Contributors
+" Author: Shidong Wang < wsdjeg at 163.com >
+" URL: https://github.com/wsdjeg/FlyGrep.vim
+" License: MIT license
+"=============================================================================
+
+""
+" @section Introduction, intro
+" @stylized FlyGrep
+" @library
+" @order intro version dicts functions exceptions layers api faq
+" Fly grep in vim, written in pure vim script for MacVim, gvim and vim version
+" 8.0+.
+"
+
+""
+" @section CONFIGURATION, config
+" FlyGrep has strong default options, but you can also change the option
+" yourself.
+
+""
+" FlyGrep will start to searching code after a delay, the default value is
+" 500ms.
+let g:FlyGrep_input_delay = 500
+
+""
+" A list of searching tools will be userd.
+let g:FlyGrep_search_tools = ['ag', 'rg', 'grep', 'pt', 'ack']
+
+let g:spacevim_data_dir
+      \ = $XDG_CACHE_HOME != ''
+      \   ? $XDG_CACHE_HOME . SpaceVim#api#import('file').separator
+      \   : expand($HOME. join(['', '.cache', ''],
+      \     SpaceVim#api#import('file').separator))
+
+""
+" Enable FlyGrep statusline
+let g:FlyGrep_enable_statusline = 1
+
+""
+" Set FlyGrep default command prompt
+let g:spacevim_commandline_prompt = '➭'
+
+command! -nargs=0 FlyGrep call FlyGrep#open({})
+EOT
             git add .
             git config user.email "wsdjeg@qq.com"
             git config user.name  "SpaceVimBot"
