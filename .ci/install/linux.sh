1d6f3722e302d182d8169ef957f62a5b58e7ebcf
Shidong Wang <wsdjeg@outlook.com>
2021-08-11 01:32:22 +0800

    Fix

--- .ci/install/linux.sh
+++ .ci/install/linux.sh
@@ -40,6 +40,8 @@ install_nvim() {
         CMAKE_BUILD_TYPE=Release \
         CMAKE_EXTRA_FLAGS="-DTRAVIS_CI_BUILD=ON -DCMAKE_INSTALL_PREFIX:PATH=$out"
     make install
+    # fix ModuleNotFoundError: No module named 'setuptools'
+    python3 -m pip install -U setuptools
     python3 -m pip install pynvim
 }
 
