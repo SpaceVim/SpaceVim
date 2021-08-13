cfdbef5f0c34c1f1a566ff9fe7cde8e5626bcd41
Shidong Wang <wsdjeg@outlook.com>
2021-08-13 22:14:41 +0800

    Update

--- lua/spacevim/api/system.lua
+++ lua/spacevim/api/system.lua
@@ -10,7 +10,7 @@ function M.name()
     if M.isLinux then
         return 'linux'
     elseif M.isWindows then
-        if vim.fn.has('win32unix') == 1 then
+        if has('win32unix') == 1 then
             return 'cygwin'
         else
             return 'windows'
@@ -20,8 +20,25 @@ function M.name()
     end
 end
 
+local is_darwin = nil
 function M.isDarwin()
-
+    if is_darwin ~= nil then
+        return is_darwin
+    end
+    if has('macunix') == 1 then
+        is_darwin = true
+        return is_darwin
+    end
+    if has('unix') ~= 1 then
+        is_darwin = false
+        return is_darwin
+    end
+    if vim.fn.system('uname -s') == "Darwin\n" then
+        is_darwin = true
+    else
+        is_darwin = false
+    end
+    return is_darwin
 end
 
 function M.fileformat()
