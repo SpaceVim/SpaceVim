c34e504693c3c9edf7b3c05df0d7e92828b83f9f
Shidong Wang <wsdjeg@outlook.com>
2021-08-13 17:06:36 +0800

    Update

--- lua/spacevim/api/system.lua
+++ lua/spacevim/api/system.lua
@@ -2,5 +2,13 @@ local M = {}
 
 M.isWindows = vim.fn.has('win16')
 
+function M.name()
+    if self.isLinux then
+        return 'linux'
+    elseif self.isWindows then
+    else
+    end
+end
+
 return M
 
