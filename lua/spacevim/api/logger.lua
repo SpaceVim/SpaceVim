ae2bf51387240b177ed8d8ac46b7bd214390393a
Shidong Wang <wsdjeg@outlook.com>
2021-08-14 14:31:04 +0800

    Add lua logger api

---
+++ lua/spacevim/api/logger.lua
@@ -0,0 +1,55 @@
+local M = {
+    ['name'] = '',
+    ['silent'] = 1,
+    ['level'] = 1,
+    ['verbose'] = 1,
+    ['file'] = '',
+    ['temp'] = {},
+}
+
+
+local levels = {'Info', 'Warn', 'Error'}
+
+function M.set_silent(sl)
+    M.silent = sl
+end
+
+function M.set_verbose(vb)
+    M.verbose = vb
+end
+
+function M.set_level(l)
+    M.level = l
+end
+
+function M.error(msg)
+    
+end
+
+function M.write(msg)
+    
+end
+
+function M.warn(msg, ...)
+    
+end
+
+function M.info(msg)
+    
+end
+
+function M.set_name(name)
+    M.name = name
+end
+
+function M.get_name()
+    return M.name
+end
+
+function M.set_file(file)
+    M.file = file
+end
+
+
+
+return M
