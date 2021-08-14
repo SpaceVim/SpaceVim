e2f73a2f2d57c941e2f625ec1a0ae4be24b50808
Shidong Wang <wsdjeg@outlook.com>
2021-08-14 08:25:45 +0800

    Update

--- lua/spacevim.lua
+++ lua/spacevim.lua
@@ -23,21 +23,21 @@ end
 
 -- this is for Vim and old neovim
 M.fn = setmetatable({}, {
-  __index = function(t, key)
-    local _fn
-    if vim.api ~= nil and vim.api[key] ~= nil then
-      _fn = function()
-        error(string.format("Tried to call API function with vim.fn: use vim.api.%s instead", key))
-      end
-    else
-      _fn = function(...)
-        return vim.call(key, ...)
-      end
-    end
-    t[key] = _fn
-    return _fn
-  end
-})
+        __index = function(t, key)
+            local _fn
+            if vim.api ~= nil and vim.api[key] ~= nil then
+                _fn = function()
+                    error(string.format("Tried to call API function with vim.fn: use vim.api.%s instead", key))
+                end
+            else
+                _fn = function(...)
+                    vim.api.nvim_call_function(key, ...)
+                end
+            end
+            t[key] = _fn
+            return _fn
+        end
+    })
 
 -- this function is only for vim
 function M.has(feature)
