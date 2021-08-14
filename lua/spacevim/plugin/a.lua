e1239b8ffdd08a132a93f374031dc742f235ebd7
Shidong Wang <wsdjeg@outlook.com>
2021-08-14 15:33:48 +0800

    Update

--- lua/spacevim/plugin/a.lua
+++ lua/spacevim/plugin/a.lua
@@ -1,5 +1,8 @@
 local M = {}
 local sp = require('spacevim')
+local cmp = require('spacevim.api').import('vim.compatible')
+local sp_file = require('spacevim.api').import('file')
+local logger = require('spacevim.logger').derive('a.vim')
 
 local alternate_conf = {}
 alternate_conf['_'] = '.project_alt.json'
