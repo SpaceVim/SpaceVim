fa26ee483f0caa8e2d0b33d37279961e0d7714fd
Shidong Wang <wsdjeg@outlook.com>
2021-08-15 19:47:37 +0800

    Update

---
+++ lua/spacevim/plugin/projectmanager.lua
@@ -0,0 +1,13 @@
+--=============================================================================
+-- projectmanager.lua --- The lua version of projectmanager..vim
+-- Copyright (c) 2016-2019 Wang Shidong & Contributors
+-- Author: Wang Shidong < wsdjeg@outlook.com >
+-- URL: https://spacevim.org
+-- License: GPLv3
+--=============================================================================
+
+
+local M = {}
+
+
+return M
