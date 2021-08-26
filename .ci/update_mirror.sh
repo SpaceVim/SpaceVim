54e3bb4b2eccb11ce8c1ef854543eb9c03d961a5
Shidong Wang <wsdjeg@outlook.com>
2021-08-26 20:59:22 +0800

    Update

--- .ci/update_mirror.sh
+++ .ci/update_mirror.sh
@@ -10,6 +10,18 @@ check_remote () {
     fi
 }
 
+push_gitee()
+{
+    git remote add gitee https://SpaceVimBot:${BOTSECRET}@gitee.com/spacevim/SpaceVim.git
+    git push gitee master 
+}
+
+push_coding()
+{
+    git remote add coding https://spacevim%40outlook.com:${CODINGBOTSECRET}@e.coding.net/spacevim/SpaceVim.git
+    git push coding master 
+}
+
 main () {
     if [ $# -gt 0 ]
         check_remote
@@ -17,6 +29,14 @@ main () {
         case $1 in
             --help|-h)
                 usage
+                exit 0
+                ;;
+            gitee)
+                push_gitee
+                exit 0
+                ;;
+            coding)
+                push_coding
                 exit 0
                 ;;
         esac
