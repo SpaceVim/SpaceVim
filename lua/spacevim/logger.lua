dc93c46f6def9d83a30b4d9613abdb48f7688fca
Wang Shidong <wsdjeg@outlook.com>
2021-08-29 16:37:19 +0800

    Update issue template (#4446)

--- lua/spacevim/logger.lua
+++ lua/spacevim/logger.lua
@@ -3,6 +3,7 @@ local M = {}
 local logger = require('spacevim.api').import('logger')
 local cmd = require('spacevim').cmd
 local call = require('spacevim').call
+local echo = require('spacevim').echo
 local fn = nil
 if vim.fn == nil then
     fn = require('spacevim').fn
@@ -57,7 +58,8 @@ function M.viewRuntimeLog()
     -- M.syntax_extra()
 end
 
-function M.viewLog(bang)
+function M.viewLog(...)
+    local argvs={...}
     local info = "<details><summary> SpaceVim debug information </summary>\n\n"
     .. "### SpaceVim options :\n\n"
     .. "```toml\n"
@@ -74,17 +76,23 @@ function M.viewLog(bang)
     .. "```log\n"
     .. logger.view(logger.level)
     .. "\n```\n</details>\n\n"
-    if bang == 1 then
-        cmd('tabnew')
-        cmd('setl nobuflisted')
-        cmd('nnoremap <buffer><silent> q :tabclose!<CR>')
-        -- put info into buffer
-        fn.append(0, fn.split(info, "\n"))
-        cmd('setl nomodifiable')
-        cmd('setl buftype=nofile')
-        cmd('setl filetype=markdown')
+    if argvs ~= nil and #argvs >= 1 then
+        local bang = argvs[1]
+        if bang == 1 then
+            cmd('tabnew')
+            cmd('setl nobuflisted')
+            cmd('nnoremap <buffer><silent> q :tabclose!<CR>')
+            -- put info into buffer
+            fn.append(0, fn.split(info, "\n"))
+            cmd('setl nomodifiable')
+            cmd('setl buftype=nofile')
+            cmd('setl filetype=markdown')
+        else
+            echo(info)
+        end
+    else
+        return info
     end
-    return info
 end
 
 function M.syntax_extra()
