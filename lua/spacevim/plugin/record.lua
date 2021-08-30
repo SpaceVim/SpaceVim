838d612b6a2e84ec713984c5ecda95a56480184e
Shidong Wang <wsdjeg@outlook.com>
2021-08-31 01:04:40 +0800

    Update

--- lua/spacevim/plugin/record.lua
+++ lua/spacevim/plugin/record.lua
@@ -8,27 +8,58 @@
 
 local M = {}
 
-M.enabled = false
+local enabled = false
+
+local keystrokes = {}
+
+
+-- timeout for remove the first key
+local remove_timeout = 3000
 
 local recordid = vim.api.nvim_create_namespace('')
 
-function M.record(char)
-    print(char)
+local borderchars = {'─', '│', '─', '│', '┌', '┐', '┘', '└'}
+
+local function draw_border(title, width, height)
+  local top = borderchars[5] ..
+        string.rep(borderchars[1], width) ..
+        borderchars[6]
+  local mid = borderchars[4] ..
+        string.rep(' ', width) ..
+        self.borderchars[2]
+  local bot = borderchars[8] ..
+        string.rep(borderchars[3], width) ..
+        borderchars[7]
+  top = string_compose(top, 1, title)
+  local lines = {top} + string.rep({mid}, height) + {bot}
+  return lines
+end
+
+local function escape(char)
+    return char
+end
+
+local function record(char)
+    table.insert(keystrokes, escape(char))
+end
+
+local function display()
+    
 end
 
 
-function M.enable()
-    vim.register_keystroke_callback(M.record, recordid)
+local function enable()
+    vim.register_keystroke_callback(record, recordid)
     M.enabled = true
 end
 
-function M.disable()
+local function disable()
     vim.register_keystroke_callback(nil, recordid)
     M.enabled = false
 end
 
 function M.toggle()
-    if M.enabled then M.disable() else M.enable() end
+    if enabled then disable() else enable() end
 end
 
 
