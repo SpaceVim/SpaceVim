731db5a0914fe889e1268adce2dff5432d99223a
wsdjeg <wsdjeg@outlook.com>
2022-05-17 21:36:08 +0800

    feat(telescope): fix message extensions

--- lua/telescope/_extensions/messages.lua
+++ lua/telescope/_extensions/messages.lua
@@ -1,6 +1,9 @@
-local pickers = require("telescope.pickers")
-local finders = require("telescope.finders")
+local action_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
 local conf = require("telescope.config").values
+local entry_display = require("telescope.pickers.entry_display")
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
 
 local function prepare_output_table()
   local lines = {}
@@ -20,6 +23,17 @@ local function show_changes(opts)
       results = prepare_output_table()
     },
     sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        local entry = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        local reg = vim.fn.getreg('*')
+        vim.fn.setreg('*', entry.value)
+        vim.cmd("put *")
+        vim.fn.setreg('*', reg)
+      end)
+      return true
+    end,
   }):find()
 end
 
