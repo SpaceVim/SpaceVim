ecf55be27dbf342ab41c04f65c4b2a8b05894b7a
wsdjeg <wsdjeg@outlook.com>
2022-07-04 15:46:01 +0800

    update

--- lua/spacevim/api/prompt.lua
+++ lua/spacevim/api/prompt.lua
@@ -33,6 +33,32 @@ M._onclose = ''
 
 M._oninputpro = ''
 
+function M.open()
+    M._quit = false
+    M._build_prompt()
+    if M.__cmp.fn.empty(M._prompt.begin) == 0 then
+        M._handle_input(M._prompt.begin)
+    else
+        M._handle_input()
+    end
+end
+
+function M._c_r_mode_off(timer)
+    M._c_r_mode = false
+end
+
+function M._handle_input(...)
+    local argv = {...}
+    local begin = argv[1] or ''
+    if M.__cmp.fn.empty(begin) == 0 then
+        if M.__cmp.fn['type'](M._oninputpro) == 2 then
+        end
+        if M.__cmp.fn['type'](M._handle_fly) == 2 then
+        end
+        M._build_prompt()
+    end
+end
+
 
 function M._build_prompt()
     local ident = M.__cmp.fn['repeat'](' ', M.__cmp.win_screenpos(0)[2] - 1)
@@ -45,6 +71,10 @@ function M._clear_prompt()
         cursor = '',
         ['end'] = ''
     }
+end
+
+function M.close()
+    
 end
 
 
