ffe186dcbedff6d071a87a41ae995041407c4492
wsdjeg <wsdjeg@outlook.com>
2022-07-04 21:22:59 +0800

    update

--- lua/spacevim/api/vim/keys.lua
+++ lua/spacevim/api/vim/keys.lua
@@ -3,9 +3,15 @@ local M = {}
 
 
 function M.t(str)
-      -- https://github.com/neovim/neovim/issues/17369
-  local ret = vim.api.nvim_replace_termcodes(str, false, true, true):gsub("\128\254X", "\128")
-  return ret
+    if vim.api ~= nil and vim.api.nvim_replace_termcodes ~= nil then
+        -- https://github.com/neovim/neovim/issues/17369
+        local ret = vim.api.nvim_replace_termcodes(str, false, true, true):gsub("\128\254X", "\128")
+        return ret
+    else
+        local ret = vim.fn.execute('echo \\' .. str)
+        ret = ret:gsub('<80>', '\128')
+        return ret
+    end
 end
 
 return M
