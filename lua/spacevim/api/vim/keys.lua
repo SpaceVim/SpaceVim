9ad0423ff3bfbf45a2c0c412d0340f4bb161ea83
wsdjeg <wsdjeg@outlook.com>
2022-07-04 21:40:31 +0800

    update

--- lua/spacevim/api/vim/keys.lua
+++ lua/spacevim/api/vim/keys.lua
@@ -8,9 +8,10 @@ function M.t(str)
         local ret = vim.api.nvim_replace_termcodes(str, false, true, true):gsub("\128\254X", "\128")
         return ret
     else
-        local ret = vim.fn.execute('echon "\\' .. str .. '"')
-        ret = ret:gsub('<80>', '\128')
-        return ret
+        -- local ret = vim.fn.execute('echon "\\' .. str .. '"')
+        -- ret = ret:gsub('<80>', '\128')
+        -- return ret
+        return vim.eval(string.format('"\\%s"', str))
     end
 end
 
