b5bf21390531baf55e4e27d55b173827cff4b7ab
wsdjeg <wsdjeg@outlook.com>
2022-07-30 19:26:36 +0800

    feat(guide): add guide.lua

---
+++ lua/spacevim/plugin/guide.lua
@@ -0,0 +1,71 @@
+--!/usr/bin/lua
+
+
+local desc_lookup = {}
+
+local cached_dicts = {}
+
+
+-- the flag for guide help mode, the default is false
+local guide_help_mode = false
+
+local function create_cache()
+    desc_lookup = {}
+
+    cached_dicts = {}
+end
+
+local function create_target_dict(key)
+    
+end
+
+
+local function build_mpt(mpt)
+    
+end
+
+local function page_down()
+    
+end
+
+local function page_undo()
+    
+end
+
+local function page_up()
+    
+end
+
+
+local function winclose()
+    
+end
+
+
+local function handle_submode_mapping(cmd)
+
+    guide_help_mode = false
+
+    updateStatusline()
+
+    if cmd == 'n' then
+        page_down()
+    elseif cmd == 'p' then
+        page_up()
+    elseif cmd == 'u' then
+        page_undo()
+    else
+        winclose()
+    end
+    
+end
+
+
+local function submode_mappings(key)
+    handle_submode_mapping(key)
+end
+
+
+local function get_register()
+    
+end
