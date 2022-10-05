7569c249aa236a591951265e2931b18e3729e4ce
wsdjeg <wsdjeg@outlook.com>
2022-10-05 23:49:40 +0800

    add read/update history functions

--- lua/spacevim/plugin/flygrep.lua
+++ lua/spacevim/plugin/flygrep.lua
@@ -19,11 +19,49 @@ local timer_stop = vim.fn.timer_stop
 
 
 -- the script local values, same as s: in vim script
-local jobid = -1
-local grep_cmd = {}
 local previous_winid = -1
+local grep_expr = ''
+local grep_default_exe,
+grep_default_opt,
+grep_default_ropt,
+grep_default_expr_opt,
+grep_default_fix_string_opt,
+grep_default_ignore_case,
+grep_default_smart_case = require('spacevim.plugin.search').default_tool()
 
-local grep_default_exe
+local grep_timer_id = -1
+local preview_timer_id = -1
+local grepid = 0
+
+local function read_histroy()
+    if vim.fn.filereadable(vim.fn.expand(vim.g.spacevim_data_dir .. 'SpaceVim/flygrep_history')) == 1 then
+        local _his = vim.fn.json_decode(vim.fn.join(vim.fn.readfile(vim.fn.expand(vim.g.spacevim_data_dir .. 'SpaceVim/flygrep_history'), ''), ''))
+        if vim.fn.type(_his) == 3 then
+            return _his
+        else
+            return {}
+        end
+    else
+        return {}
+    end
+end
+
+local function update_history()
+    if vim.fn.index(grep_history, grep_expr) >= 0 then
+        vim.fn.remove(grep_history, vim.fn.index(grep_history, grep_expr))
+    end
+    vim.fn.add(grep_history, grep_expr)
+    if vim.fn.isdirectory(vim.fn.expand(vim.g.spacevim_data_dir .. 'SpaceVim')) == 0 then
+        vim.fn.mkdir(vim.fn.expand(vim.g.spacevim_data_dir .. 'SpaceVim'))
+    end
+    if vim.fn.filereadable(vim.fn.expand(vim.g.spacevim_data_dir .. 'SpaceVim/flygrep_history')) == 1 then
+        vim.fn.writefile({vim.fn.json_encode(grep_history)}, vim.fn.expand(vim.g.spacevim_data_dir .. 'SpaceVim/flygrep_history'))
+    end
+    
+end
+
+local grep_history = read_histroy()
+local complete_input_history_num = {0, 0}
 
 -- The available options are:
 -- - input: string, the default input pattern
@@ -46,12 +84,12 @@ local function flygrep(t)
     jobid = jobstart(grep_cmd, {
         on_stdout = flygrep_stdout,
     })
-    
+
 end
 
 
 function flygrep_stdout(id, data, event)
-    
+
 end
 
 
