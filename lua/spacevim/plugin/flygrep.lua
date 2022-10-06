d5792ee9a969a9af44f5bab1d1ba22fddafa3491
wsdjeg <wsdjeg@outlook.com>
2022-10-06 09:19:17 +0800

    update

--- lua/spacevim/plugin/flygrep.lua
+++ lua/spacevim/plugin/flygrep.lua
@@ -1,6 +1,7 @@
 local flygrep = {}
 
 local logger = require('spacevim.logger').derive('flygrep')
+local mpt = require('spacevim.api').import('prompt')
 
 -- the job functions
 local jobstart = vim.fn.jobstart
@@ -90,12 +91,58 @@ end
 
 local function get_search_cmd(expr)
    local cmd = {grep_exe} + grep_opt 
+   if vim.o.ignorecase then
+       cmd = cmd + grep_ignore_case
+   end
+   if vim.o.smartcase then
+       cmd = cmd + grep_smart_case
+   end
+   if grep_mode == 'string' then
+       cmd = cmd + grep_default_fix_string_opt
+   end
+   cmd = cmd + grep_expr_opt
+   if not empty(grep_files) and vim.fn.type(grep_files) == 3 then
+       cmd = cmd + {expr} + grep_files
+   elseif not empty(grep_files) and vim.fn.type(grep_files) == 1 then
+       cmd = cmd + {expr} + {grep_files}
+   elseif not empty(grep_dir) then
+       if grep_exe == 'findstr' then
+           cmd = cmd + {grep_dir} + {expr} + {[[%CD%\*]]}
+       else
+           cmd = cmd + {expr} + {grep_dir}
+       end
+   else
+       cmd = cmd + {expr}
+       if grep_exe == 'rg' or grep_exe == 'ag' or grep_exe == 'pt' then
+           cmd = cmd + {'.'}
+       end
+       cmd = cmd + grep_ropt
+   end
+   return cmd
 end
 
+local mode = ''
+local buffer_id = -1
+local flygrep_win_id = -1
 
 function flygrep.open(argv)
 
     previous_winid = vim.fn.win_getid()
+    if empty(grep_default_exe) then
+        logger.warn(' [flygrep] make sure you have one search tool in your PATH')
+        return
+    end
+    mpt._handle_fly = flygrep
+    buffer_id = vim.api.nvim_create_buf(false, true)
+    local flygrep_win_height = 16
+    flygrep_win_id = vim.api.nvim_open_win(buffer_id, true,{
+        relative = 'editor',
+        width = vim.o.columns,
+        height = flygrep_win_height,
+        row = vim.o.lines - flygrep_win_height - 2,
+        col = 0
+    })
+
 
 end
 
