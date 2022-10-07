fa7d5dc1bee26b2607c0f251c750426274013a96
wsdjeg <wsdjeg@outlook.com>
2022-10-07 10:21:02 +0800

    fix(flygrep): fix grep_stdout function

--- lua/spacevim/plugin/flygrep.lua
+++ lua/spacevim/plugin/flygrep.lua
@@ -128,7 +128,7 @@ local function grep_stdout(id, data, event)
     if id ~= grepid then return end
     local datas = vim.fn.filter(data, '!empty(v:val)')
     --  let datas = s:LIST.uniq_by_func(datas, function('s:file_line'))
-    if vim.fn.getbufline(buffer_id, 1) == '' then
+    if vim.fn.getbufline(buffer_id, 1)[1] == '' then
         vim.api.nvim_buf_set_lines(buffer_id, 0, -1, false, datas)
     else
         vim.api.nvim_buf_set_lines(buffer_id, -1, -1, false, datas)
