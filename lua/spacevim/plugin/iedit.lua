d5d4f8f3c3f673bedea78420a1ca458cab285ac5
wsdjeg <wsdjeg@outlook.com>
2022-10-15 21:51:51 +0800

    handle insert key: c-w c-u c-k and bs/c-h

--- lua/spacevim/plugin/iedit.lua
+++ lua/spacevim/plugin/iedit.lua
@@ -373,6 +373,33 @@ local function handle_insert(char) -- {{{
     return cursor_stack[1].cursor_begin
       .. cursor_stack[1].cursor_char
       .. cursor_stack[1].cursor_end
+  elseif char == k.t('<C-w>') then
+    for i = 1, #cursor_stack, 1 do
+      if cursor_stack[i].active then
+        cursor_stack[i].cursor_begin =
+          substitute(cursor_stack[i].cursor_begin, [[\S*\s*$]], '', 'g')
+      end
+    end
+  elseif char == k.t('<C-u>') then
+    for i = i, #cursor_stack, 1 do
+      if cursor_stack[i].active then
+        cursor_stack[i].cursor_begin = ''
+      end
+    end
+  elseif char == k.t('<C-k>') then
+    for i = i, #cursor_stack, 1 do
+      if cursor_stack[i].active then
+        cursor_stack[i].cursor_char = ''
+        cursor_stack[i].cursor_end = ''
+      end
+    end
+  elseif char == k.t('<bs>') or char == k.t('<C-h>') then
+    for i = i, #cursor_stack, 1 do
+      if cursor_stack[i].active then
+        cursor_stack[i].cursor_begin =
+          substitute(cursor_stack[i].cursor_begin, '.$', '', 'g')
+      end
+    end
   else
     for i = 1, #cursor_stack, 1 do
       if cursor_stack[i].active then
