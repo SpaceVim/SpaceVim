89a52dc31d613e4c100a8e2a4afc2325ebb10174
wsdjeg <wsdjeg@outlook.com>
2022-10-25 10:56:00 +0800

    update

--- lua/telescope/_extensions/zettelkasten_template.lua
+++ lua/telescope/_extensions/zettelkasten_template.lua
@@ -4,38 +4,39 @@ local conf = require("telescope.config").values
 local entry_display = require("telescope.pickers.entry_display")
 local finders = require("telescope.finders")
 local pickers = require("telescope.pickers")
-local sp_file = require('spacevim.api').import('file')
 local zk_config = require("zettelkasten.config")
 
 local function prepare_output_table()
     local lines = {}
-    local result = vim.split(vim.fn.globpath(zk_config.templete_dir, '**/*'), "\n")
+    local result = vim.fn.globpath(zk_config.templete_dir, '**/*', 0, 1)
 
-    for templete in ipairs(result) do
+    for _, templete in ipairs(result) do
       table.insert(lines, templete)
     end
     return lines
 end
 
 local function show_script_names(opts)
-    opts = opts or {}
-    pickers.new(opts, {
-        prompt_title = "ZettelKasten Templete",
-        finder = finders.new_table {
-            results = prepare_output_table()
-        },
-        sorter = conf.generic_sorter(opts),
-        attach_mappings = function(prompt_bufnr, map)
-            actions.select_default:replace(function()
-                actions.close(prompt_bufnr)
-                local entry = action_state.get_selected_entry()
-                -- print(vim.inspect(selection))
-                -- vim.cmd("e " + entry.value)
-                vim.cmd('e ' .. vim.fn.split(entry.value, ": ")[2])
-            end)
-            return true
-        end,
-    }):find()
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "ZettelKasten Templete",
+    finder = finders.new_table {
+      results = prepare_output_table()
+    },
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr)
+      actions.select_default:replace(function()
+        local entry = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        -- the file name is entry.value
+        -- do something with the file
+        vim.cmd('ZkNew')
+        local templete_context = vim.fn.readfile(entry.value, '')
+        vim.api.nvim_buf_set_lines(0, 0, -1, false, templete_context)
+      end)
+      return true
+    end,
+  }):find()
 end
 
 local function run()
