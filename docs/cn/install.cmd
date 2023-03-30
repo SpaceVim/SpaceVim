390345e3292e77924e6e66f1ca6f239158511bba
wsdjeg <wsdjeg@outlook.com>
2023-03-30 20:43:41 +0800

    chore(version): update to v2.2.0-dev

--- docs/cn/install.cmd
+++ docs/cn/install.cmd
@@ -11,7 +11,7 @@ echo "       \______/| ##____/ \_______/\_______/\_______/   \_/   |__|__/ |__/ 
 echo "               | ##                                                           "
 echo "               | ##                                                           "
 echo "               |__/                                                           "
-echo "                    版本: 2.1.0        中文官网: https://spacevim.org/cn/     "
+echo "                    版本: 2.2.0-dev    中文官网: https://spacevim.org/cn/     "
 
 Push-Location ~
 
@@ -119,5 +119,3 @@ echo "感谢支持 SpaceVim，欢迎反馈！"
 echo ""
 
 Pause
-
-# vim:set ft=powershell nowrap: 
--- docs/install.cmd
+++ docs/install.cmd
@@ -19,7 +19,7 @@ echo "       \______/| ##____/ \_______/\_______/\_______/   \_/   |__|__/ |__/ 
 echo "               | ##                                                           "
 echo "               | ##                                                           "
 echo "               |__/                                                           "
-echo "                      version : v2.1.0         by : spacevim.org              "
+echo "                      version : v2.2.0-dev     by : spacevim.org              "
 
 Function Pause ($Message = "Press any key to continue . . . ") {
     if ((Test-Path variable:psISE) -and $psISE) {
@@ -140,4 +140,3 @@ echo ""
 echo "That's it. Thanks for installing $app_name. Enjoy!"
 echo ""
 Pause
-
