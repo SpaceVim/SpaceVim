99f3bf633908d6945fa9f5c58b2ee57634845b31
wsdjeg <wsdjeg@outlook.com>
2023-04-26 23:43:08 +0800

    add toml api

---
+++ lua/spacevim/api/data/toml.lua
@@ -0,0 +1,25 @@
+--=============================================================================
+-- toml.lua --- toml lua api
+-- Copyright (c) 2016-2023 Wang Shidong & Contributors
+-- Author: Wang Shidong < wsdjeg@outlook.com >
+-- URL: https://spacevim.org
+-- License: GPLv3
+--=============================================================================
+
+
+local M = {}
+
+function! s:self.parse_file(filename) abort
+
+
+function M.parse(text) -- {{{
+  
+end
+-- }}}
+
+function M.parse_file(filename) -- {{{
+  
+end
+-- }}}
+
+return M
