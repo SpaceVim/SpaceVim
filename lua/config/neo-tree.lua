c269420878dca12b67ed0041b53d3715694ba8a8
wsdjeg <wsdjeg@outlook.com>
2023-06-11 23:10:26 +0800

    perf(neo-tree): use libuv file watcher

--- lua/config/neo-tree.lua
+++ lua/config/neo-tree.lua
@@ -160,7 +160,7 @@ require('neo-tree').setup({
     -- "open_current",  -- netrw disabled, opening a directory opens within the
     -- window like netrw would, regardless of window.position
     -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-    use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+    use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
     -- instead of relying on nvim autocmd events.
     window = {
       mappings = {
