5efe1681980a655841fceb99e1b26f569fc9e8a1
wsdjeg <wsdjeg@outlook.com>
2023-06-22 22:00:01 +0800

    add notify api

---
+++ lua/spacevim/api/notify.lua
@@ -0,0 +1,20 @@
+--=============================================================================
+-- notify.lua --- notift api for spacevim
+-- Copyright (c) 2016-2023 Wang Shidong & Contributors
+-- Author: Wang Shidong < wsdjeg@outlook.com >
+-- URL: https://spacevim.org
+-- License: GPLv3
+--=============================================================================
+
+
+local M = {}
+
+---@param msg string|table<string> notification messages
+---@param opts table notify options
+---  - title: string, the notify title
+function M.notify(msg, opts) -- {{{
+  
+end
+-- }}}
+
+return M
