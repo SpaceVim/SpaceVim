bcf72ff5d37f2d5501085e86dfd118c56a35e930
wsdjeg <wsdjeg@outlook.com>
2023-06-28 13:46:43 +0800

    update

--- lua/spacevim/api/job.lua
+++ lua/spacevim/api/job.lua
@@ -5,3 +5,44 @@
 -- URL: https://spacevim.org
 -- License: GPLv3
 --=============================================================================
+
+local uv = vim.loop
+
+local M = {}
+
+-- s:self.start(argv,...)
+-- s:self.stop(id)
+-- s:self.send(id,data)
+-- s:self.status(id)
+-- s:self.list()
+-- s:self.info(id)
+-- s:self.chanclose(id,type)
+-- s:self.debug()
+function M.start(argv, ...) -- {{{
+end
+-- }}}
+
+function M.stop(id) -- {{{
+end
+-- }}}
+
+function M.send(id, data) -- {{{
+end
+-- }}}
+
+function M.status(id) -- {{{
+end
+-- }}}
+
+function M.list() -- {{{
+end
+-- }}}
+function M.info(id) -- {{{
+end
+-- }}}
+
+function M.chanclose(id, t) -- {{{
+end
+-- }}}
+
+return M
