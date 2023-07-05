d087d157254f37c5a457d243e1be7fc8f410d563
wsdjeg <wsdjeg@outlook.com>
2023-07-05 21:20:26 +0800

    update

--- test/lua/test_job.lua
+++ test/lua/test_job.lua
@@ -1,14 +1,14 @@
 local job = require('spacevim.api.job')
 
-job.start({ 'cat', '--version' }, {
+job.start({ 'vim', '--version' }, {
   on_stdout = function(id, data, event)
     vim.print(id)
     vim.print(data)
     vim.print(event)
   end,
-  on_exit = function(id, code, singin)
+  on_exit = function(id, code, signal)
     vim.print(id)
-    vim.print(data)
-    vim.print(event)
+    vim.print('exit code', code)
+    vim.print('exit signal', signal)
   end,
 })
