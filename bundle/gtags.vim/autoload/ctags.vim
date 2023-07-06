6aac7db822efc2ea8df8eb70b89204a67b4dc741
wsdjeg <wsdjeg@outlook.com>
2023-07-06 15:08:49 +0800

    add ctags plugin

--- bundle/gtags.vim/autoload/ctags.vim
+++ bundle/gtags.vim/autoload/ctags.vim
@@ -11,6 +11,13 @@ scriptencoding utf-8
 if exists('g:loaded_ctags')
   finish
 endif
+let g:loaded_ctags = 1
+if has('nvim-0.7.0') && 0
+  function! ctags#update(...) abort
+    lua require('ctags').update()
+  endfunction
+  finish
+endif
 
 
 let s:LOGGER =SpaceVim#logger#derive('ctags')
@@ -27,7 +34,6 @@ endif
 let s:JOB = SpaceVim#api#import('job')
 let s:FILE = SpaceVim#api#import('file')
 
-let g:loaded_ctags = 1
 
 let s:is_u_ctags = 0
 let s:version_checked = 0
---
+++ bundle/gtags.vim/lua/ctags/init.lua
@@ -0,0 +1,21 @@
+--=============================================================================
+-- init.lua --- ctags plugin
+-- Copyright (c) 2016-2023 Wang Shidong & Contributors
+-- Author: Wang Shidong < wsdjeg@outlook.com >
+-- URL: https://spacevim.org
+-- License: GPLv3
+--=============================================================================
+
+
+local M = {}
+
+local log = require('spacevim.logger').derive('ctags')
+
+
+
+function M.update() -- {{{
+  
+end
+-- }}}
+
+return M
