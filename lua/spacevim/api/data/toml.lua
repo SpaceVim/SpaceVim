1bef7445f31eaea6c4677631692d45191ff5106b
wsdjeg <wsdjeg@outlook.com>
2023-07-13 18:05:00 +0800

    update

--- lua/spacevim/api/data/toml.lua
+++ lua/spacevim/api/data/toml.lua
@@ -14,9 +14,50 @@ function M.parse(text) -- {{{
 end
 -- }}}
 
-function M.parse_file(filename) -- {{{
+function M.parse_file(filename)
   
 end
--- }}}
+
+function M._consume(input, pattern)
+  M._skip(input)
+  local _end = vim.fn.matchend(input.text, pattern, input.p)
+  if _end == -1 then
+    M._error(input)
+  elseif _end == input.p then
+    return ''
+  end
+  local matched = vim.fn.strpat(input.text, input.p, _end - input.p)
+  input.p = _end
+  return matched
+end
+
+
+local skip_pattern = '\\C^\\%(\\_s\\+\\|#[^\r\n]*\\)'
+
+function M._skip(input)
+  while M._match(input, [[\%(\_s\|#\)]]) do
+    input.p = vim.fn.matchend(input.text, skip_pattern, input.p)
+  end
+end
+
+function M._match(input, pattern)
+  return vim.fn.match(input.text, pattern, input.p) ~= -1
+end
+
+function M._eof(input)
+  return input.p >= input.length
+end
+
+function M._error(input)
+  local buf = {}
+  local offset = 0
+  -- @todo !=# string to lua
+  while (input.p + offset) < input.length and input.text[input.p + offset] ~= "[\r\n]" do
+    table.insert(buf,  input.text[input.p + offset])
+    offset = offset + 1
+  end
+
+  
+end
 
 return M
