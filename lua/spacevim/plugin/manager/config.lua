e8617d495f04776a6d195ca2b7d5b1c3b4966697
wsdjeg <wsdjeg@outlook.com>
2023-07-16 17:31:55 +0800

    update

---
+++ lua/spacevim/plugin/manager/config.lua
@@ -0,0 +1,6 @@
+--!/usr/bin/lua
+local M = {}
+
+M.base_dir = vim.fn.stdpath('data')
+
+return M
---
+++ lua/spacevim/plugin/manager/git.lua
@@ -0,0 +1,13 @@
+local M  = {}
+
+local conf = require('spacevim.plugin.manager.config')
+
+function M.get_clone_cmd(url, dir)
+
+  local cmd = {'git', 'clone'}
+  
+end
+
+return M
+
+
