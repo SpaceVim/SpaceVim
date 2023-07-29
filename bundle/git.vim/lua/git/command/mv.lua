487f36ce3156c94a20a5b1e94670ba6803bda608
wsdjeg <wsdjeg@outlook.com>
2023-07-29 15:48:52 +0800

    perf(git): display stderr on exit

--- bundle/git.vim/lua/git/command/mv.lua
+++ bundle/git.vim/lua/git/command/mv.lua
@@ -3,28 +3,38 @@ local m = {}
 local job = require('spacevim.api.job')
 local nt = require('spacevim.api.notify')
 local log = require('git.log')
+local stddata = {}
 
 local function on_exit(id, code, single)
   log.debug('git-mv exit code:' .. code .. ' single:' .. single)
   if code == 0 and single == 0 then
     nt.notify('git mv successfully')
   else
-    nt.notify('failed to run git mv', 'warningmsg')
+    nt.notify(table.concat(stddata, '\n'), 'warningmsg')
+  end
+end
+
+local function on_std(id, data)
+  for _, v in ipairs(data) do
+    table.insert(stddata, v)
   end
 end
 
 function m.run(argv)
-  local cmd = {'git', 'mv'}
+  local cmd = { 'git', 'mv' }
   if vim.fn.index(argv, '%') ~= -1 then
     argv[vim.fn.index(argv, '%') + 1] = vim.fn.expand('%')
   end
-  for _, v in ipairs(argv) do table.insert(cmd, v)
+  for _, v in ipairs(argv) do
+    table.insert(cmd, v)
   end
   log.debug('git-mv cmd:' .. vim.inspect(cmd))
+  stddata = {}
   job.start(cmd, {
-    on_exit = on_exit
+    on_exit = on_exit,
+    on_stdout = on_std,
+    on_stderr = on_std
   })
 end
 
 return m
-
