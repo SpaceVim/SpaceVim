61faabd18a9d7a6e1767d1e4453a33503751afa2
wsdjeg <wsdjeg@outlook.com>
2023-08-06 11:13:23 +0800

    update

--- lua/spacevim/api/cmdlinemenu.lua
+++ lua/spacevim/api/cmdlinemenu.lua
@@ -28,11 +28,16 @@ local function parse_items(items)
   local is = {}
   local id = 1
   for _, item in ipairs(items) do
-    is['' .. id] = item
-    is['' .. id][1] = '(' .. id .. ')' .. item[1]
+    is[id] = item
+    is[id][1] = '(' .. id .. ')' .. item[1]
     id = id + 1
   end
   return is
+end
+
+local function list_keys(l)
+  if #l == 0 then return {} end
+  return vim.fn.range(1, #l)
 end
 
 function M.menu(items)
@@ -43,17 +48,19 @@ function M.menu(items)
   items = parse_items(items)
   vim.o.cmdheight = #items + 1
   vim.cmd('redrawstatus!')
-  local selected = '1'
+  local selected = 1
   local exit = false
   local indent = string.rep(' ', 7, '')
   while not exit do
     local menu = 'Cmdline menu: Use j/k/enter and the shortcuts indicated\n'
-    for id, _ in pairs(items) do
+    local id = 1
+    for _, _ in pairs(items) do
       if id == selected then
         menu = menu .. indent .. '>' .. items[id][1] .. '\n'
       else
         menu = menu .. indent .. ' ' .. items[id][1] .. '\n'
       end
+      id = id + 1
     end
 
     vim.cmd('redraw!')
@@ -63,9 +70,9 @@ function M.menu(items)
       exit = true
       cancelled = true
       vim.cmd('normal! :')
-    elseif vim.fn.index(vim.fn.keys(items), char) ~= -1 or char == Key.t('<Cr>') then
+    elseif vim.fn.index(list_keys(items), tonumber(char)) ~= -1 or char == Key.t('<Cr>') then
       if char ~= Key.t('<Cr>') then
-        selected = char
+        selected = tonumber(char, 10)
       end
       local value = items[selected][1]
       vim.cmd('normal! :')
@@ -77,10 +84,10 @@ function M.menu(items)
       end
       exit = true
     elseif char == 'j' or char == Key.t('<Tab>') then
-      selected = next_item(vim.fn.keys(items), selected)
+      selected = next_item(list_keys(items), selected)
       vim.cmd('normal! :')
     elseif char == 'k' or char == Key.t('<S-Tab>') then
-      selected = previous_item(vim.fn.keys(items), selected)
+      selected = previous_item(list_keys(items), selected)
       vim.cmd('normal! :')
     else
       vim.cmd('normal! :')
