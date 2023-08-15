83f848c947091de8ff38f91250ce3aa32e94a13b
wsdjeg <wsdjeg@outlook.com>
2023-08-15 20:41:13 +0800

    update lua guide

--- lua/spacevim/plugin/guide.lua
+++ lua/spacevim/plugin/guide.lua
@@ -1,5 +1,10 @@
---!/usr/bin/lua
-
+--=============================================================================
+-- guide.lua --- Key binding guide for spacevim
+-- Copyright (c) 2016-2023 Wang Shidong & Contributors
+-- Author: Wang Shidong < wsdjeg@outlook.com >
+-- URL: https://spacevim.org
+-- License: GPLv3
+--=============================================================================
 
 local M = {}
 
