7de1a232e1daccb2e7ff327da8481ee8a4b20a86
wsdjeg <wsdjeg@outlook.com>
2023-08-26 20:03:35 +0800

    update

--- lua/spacevim/plugin/repl.lua
+++ lua/spacevim/plugin/repl.lua
@@ -10,7 +10,35 @@ local job = require('spacevim.api.job')
 
 local log = require('spacevim.logger').derive('repl')
 
+local lines = 0
+local bufnr = -1
+local status = {}
+local start_time
+
 local M = {}
+
+local function open_windows()
+  if vim.api.nvim_buf_is_valid(bufnr) then
+    vim.cmd('bd ' .. bufnr)
+  end
+  vim.cmd('botright split __REPL__')
+  local l = math.floor(vim.o.lines * 30 / 100)
+  vim.cmd('resize ' .. l)
+end
+
+local function start(exe)
+  lines = 0
+  status = {
+    is_running = true,
+    is_exit = false,
+    has_errors = false,
+    exit_code = 0
+  }
+
+  start_time = vim.fn.reltime()
+  open_windows()
+  vim.api.nvim_buf_set_lines(bufnr, lines, lines + 3, false, {'[REPL executable] ' .. vim.fn.string(exe), '', string.rep('-', 20)})
+end
 
 
 function M.start(ft)
