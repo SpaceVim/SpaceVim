123a219e0d6e2a9101b7d78987edd1c2361d23ea
wsdjeg <wsdjeg@outlook.com>
2023-08-26 19:52:15 +0800

    add repl.lua

---
+++ lua/spacevim/plugin/repl.lua
@@ -0,0 +1,20 @@
+--=============================================================================
+-- repl.lua --- REPL for spacevim
+-- Copyright (c) 2016-2022 Wang Shidong & Contributors
+-- Author: Wang Shidong < wsdjeg@outlook.com >
+-- URL: https://spacevim.org
+-- License: GPLv3
+--=============================================================================
+
+local job = require('spacevim.api.job')
+
+local log = require('spacevim.logger').derive('repl')
+
+local M = {}
+
+
+function M.start(ft)
+  
+end
+
+return M
