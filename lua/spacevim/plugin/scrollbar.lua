bba74759fc6aacd5b3b3b5c203aee066b5a857d8
wsdjeg <wsdjeg@outlook.com>
2023-09-17 10:34:56 +0800

    update

--- lua/spacevim/plugin/scrollbar.lua
+++ lua/spacevim/plugin/scrollbar.lua
@@ -6,15 +6,33 @@
 -- License: GPLv3
 --=============================================================================
 
+local M = {}
+
 local default_conf = {
   max_size = 10,
   min_size = 3,
   width = 1,
   right_offset = 1,
-  excluded_filetypes = {'startify', 'git-commit','leaderf', 'NvimTree', 'tagbar', 'defx', 'neo-tree', 'qf'}
+  excluded_filetypes = {'startify', 'git-commit','leaderf', 'NvimTree', 'tagbar', 'defx', 'neo-tree', 'qf'},
   shape = {
     head = '▲',
     body = '█',
     tail = '▼'
+  },
+  highlight = {
+    head = 'Normal',
+    body = 'Normal',
+    tail = 'Normal'
   }
 }
+
+local scrollbar_bufnr = -1
+local scrollbar_winid = -1
+local scrollbar_size = -1
+
+
+function M.show()
+  
+end
+
+return M
