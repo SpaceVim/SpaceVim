56d1f349176b601839ee1a80f9ae0cb0777e46b7
Eric Wong <eric@wsdjeg.net>
2024-02-23 14:44:38 +0800

    fix(git): skip unsupported subcommand

--- bundle/git.vim/lua/git/init.lua
+++ bundle/git.vim/lua/git/init.lua
@@ -2,7 +2,48 @@ local M = {}
 
 local log = require('git.log')
 
+local cmds = {
+  'add',
+  'blame',
+  'branch',
+  'checkout',
+  'cherry-pick',
+  'clean',
+  'commit',
+  'diff',
+  'fetch',
+  'log',
+  'merge',
+  'mv',
+  'pull',
+  'push',
+  'remote',
+  'reset',
+  'rm',
+  'shortlog',
+  'status',
+}
+local supported_commands = {}
+
+local function update_cmd()
+  for _, v in ipairs(cmds) do
+    supported_commands[v] = true
+  end
+end
+
+update_cmd()
+
+
 function M.run(command, ...)
+
+  if not supported_commands[command] then
+      vim.api.nvim_echo(
+        { { ':Git ' .. command .. ' is not supported', 'WarningMsg' } },
+        false,
+        {}
+      )
+    return
+  end
   local argv = { ... }
   local ok, cmd = pcall(require, 'git.command.' .. command)
   if ok then
