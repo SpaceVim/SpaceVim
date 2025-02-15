e47c3a3b9931f8f56547ed883a8928b943be4e85
Eric Wong <wsdjeg@outlook.com>
2025-02-15 13:28:36 +0800

    fix(ci): fix detach_plugin func

--- .ci/detach_plugin.sh
+++ .ci/detach_plugin.sh
@@ -151,7 +151,7 @@ main () {
             _detact_bundle $1 lua/plug/config.lua
             _detact_bundle $1 lua/plug/hooks.lua
             _detact_bundle $1 lua/plug/init.lua
-            _detact_bundle $1 lua/plug/instealler.lua
+            _detact_bundle $1 lua/plug/installer.lua
             _detact_bundle $1 lua/plug/loader.lua
             _detact_bundle $1 lua/plug/ui.lua
             _checkdir test
